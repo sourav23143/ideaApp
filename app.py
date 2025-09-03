6b430edfba9c2fcfb7666bf39a365fdf81ff8d2e
unknown <sourav009966@gmail.com>
2025-09-04 00:21:06 +0530

    Post api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+#request use to track request body
 
 app = Flask(__name__) 
 
@@ -28,6 +29,36 @@ def get_all_ideas():
     #logic to fetch all the ideas
     return ideas   #so this is our control as inside it logic is written to fetch the ideas
 
+'''
+Create a RESTful endpoint for creating a new idea
+'''
+@app.post("/ideaapp/api/v1/ideas")
+def create_idea():
+    #logic to create a new idea
+
+
+    #whenverr we created something like this there is a chance that there might be some error in it so we use try- except 
+    try:
+
+        #first read the request body 
+        request_body = request.get_json()
+
+
+        #check if the idea id passed is not present already
+        if request_body["id"] and request_body["id"] in ideas:
+            return "idea with same id already present",400
+    
+
+        #Insert the passed idea in the ideas dictonary
+        ideas[request_body["id"]] = request_body
+
+        #return the response saying idea got saved
+        return "idea created and saved sucessfully",201
+    except KeyError:
+        return "id is missing",400
+    except:
+        return "Some internal server error", 500
+    
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(port=5000 )
